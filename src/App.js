d6f51211e4754380cb50f1cb7cad5d5fc5ddc480
Samir Cousen <samirsertanejo@gmail.com>
2018-08-31 16:55:48 -0300

    Botao, input, state, metodo construtor, lista

--- src/App.js
+++ src/App.js
@@ -3,19 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        nomes: []
+      };
+    }
+
+  botao = () => {
+    console.log('funcionou');
+  }
+
+  criaLista = () => {
+    var lista =
+      <ul>
+        <li>123</li>
+        <li>123</li>
+        <li>MODA SERTANEJA</li>
+      </ul>;
+
+    return lista;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bem Vindo ao React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <input type = 'text' placeholder = "Clica aqui" style = {{padding: '20px'}}/>
+
+        <button onClick = {this.botao} style = {{
+           color: 'red',
+           backgroundColor: '#222',
+           padding: '15px'
+
+        }}>
+            Gostosa da cafeteria
+
+        </button>
+
+        <br />
+        <div>
+          {this.criaLista()}
+        </div>
+
       </div>
     );
   }
+
 }
 
 export default App;
